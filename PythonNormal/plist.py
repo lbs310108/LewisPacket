c40814d8f9ceadfd40430472b7efacab7072ca9a
lewis <lbs310108>
2020-01-26 12:58:49 +0800

    2020-1-26

---
+++ PythonNormal/plist.py
@@ -0,0 +1,8 @@
+# list duplicate remove
+def dr_list(list_name):
+    new_list = []
+    for value in list_name:
+        if value not in new_list:
+            new_list.append(value)
+    return new_list
+
