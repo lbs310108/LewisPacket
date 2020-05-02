b71699b15785a43694ce7ac369e8ce0d63a756ba
lbs310108 <838648292@qq.com>
2020-05-03 07:54:25 +0800

    add dstring.py some function

--- PythonNormal/dstring.py
+++ PythonNormal/dstring.py
@@ -1,5 +1,17 @@
-# input keywords and string, return the string after the keywords
-
-
-def after_keywords(string, keywords):
+# input keywords and string, find from last, return the string after the keywords
+def after_keywords_last(string, keywords):
     return string[string.rfind(keywords):]
+
+
+# input keywords and string, find from first, return the string after the keywords
+def after_keywords_first(string, keywords):
+    return string[string.find(keywords):]
+
+
+# input keywords and string,find from string. if find:return true;if not find:return false
+def find_keywords_in_string(string, keywords):
+    STR = string.find(keywords)
+    if STR == -1:
+        return False
+    else:
+        return True
