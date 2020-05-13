1b7656d91e70b58da29563d83d21fd99c38ecb14
lbs310108 <838648292@qq.com>
2020-05-13 22:57:55 +0800

    send packet test

--- lewis.py
+++ lewis.py
@@ -1,26 +1,33 @@
 import netifaces
-import struct
 import socket
 
 # variable
 INTERFACE = 'lo'
 
-# get local info
-# local_network_detail = netifaces.ifaddresses(INTERFACE)
-# local_ip_address = local_network_detail[2][0]['addr']
-# local_mac_address = local_network_detail[17][0]['addr']
-# bytes_local_mac_address = bytes.fromhex(local_mac_address.replace(':', ''))
+# get local MAC and IP
+local_network_detail = netifaces.ifaddresses(INTERFACE)
+local_ip_address = local_network_detail[2][0]['addr']
+local_mac_address = local_network_detail[17][0]['addr']
+bytes_local_mac_address = bytes.fromhex(local_mac_address.replace(':', ''))
+bytes_local_ip_address = bytes.fromhex(local_ip_address.replace('.', ''))
 
-# arp_hdr = struct.pack("!2s2s1s1s2s", b'\x00\x01', b'\x08\x00', b'\x06', b'\x04', b'\x00\x01')
+# define ethernet2 variable
+bytes_dest_mac_address = b'\xff\xff\xff\xff\xff\xff'
+byte_ethernet_type = b'\x08\x06'
+fr_eth = bytes_dest_mac_address + bytes_local_mac_address + byte_ethernet_type
 
-# ethernet2
-dist_mac = b'\xff\xff\xff\xff\xff\xff'
-sour_mac = b'\xff\xff\xff\xff\xff\xff'
-eth_type = b'\x00\x01'
+# define arp request header
+hardware_type = b'\x01\x00'
+protocol_type = b'\x08\x00'
+hardware_size = b'\x06'
+protocol_size = b'\x04'
+opcode = b'\x00\x01'
+arp_reqeust_header = hardware_type + protocol_type + hardware_size + protocol_size + opcode
 
-arp_hdr = dist_mac + sour_mac + eth_type
+# make arp request
+arp_packet = fr_eth + arp_reqeust_header
 
 sk = socket.socket(socket.PF_PACKET, socket.SOCK_RAW)
 sk.bind((INTERFACE, 0))
-sk.send(arp_hdr)
+sk.send(arp_packet)
 
